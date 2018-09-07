43cdd1705964e18c6f74151f2874c71e5aebc846
Aleksey A. Morozov <AlekVolsk@users.noreply.github.com>
2018-09-07 14:43:05 +0400

    Update articlefrontviewbtn.php

--- articlefrontviewbtn.php
+++ articlefrontviewbtn.php
@@ -1,32 +1,46 @@
 <?php defined( '_JEXEC' ) or die( 'Restricted access' );
 
-jimport( 'joomla.plugin.plugin' );
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Application\SiteApplication;
+use Joomla\CMS\Toolbar\Toolbar;
 
-class plgSystemArticlefrontviewbtn extends JPlugin
+class plgSystemArticlefrontviewbtn extends CMSPlugin
 {
     public function onBeforeRender()
     {
-        $app = JFactory::getApplication();
+        $app = Factory::getApplication();
 
-        if (JFactory::getDocument()->getType() != 'html')
+        if ( Factory::getDocument()->getType() != 'html' )
         {
             return false;
         }
 
-        if (!$app->isAdmin())
+        if ( !$app->isAdmin() )
         {
             return false;
         }
 
-        $option = $app->input->get('option');
-        $view   = $app->input->get('view');
-        $layout = $app->input->get('layout');
-        $id     = $app->input->get('id');
-        if ($option == 'com_content' && $view == 'article' && $layout == 'edit' && $id)
+        $option = $app->input->get( 'option' );
+        $view   = $app->input->get( 'view' );
+        $layout = $app->input->get( 'layout' );
+        $id     = $app->input->get( 'id' );
+        
+        if ( $layout == 'edit' && $id && $option && $view )
         {
-            $html = '<a href="' . JUri::root() . 'index.php?option=com_content&view=article&id=' . $id . '" target="_blank" class="btn btn-small"><span class="icon-eye"></span> See what the site looks like</a>';
-            $toolbar = JToolBar::getInstance('toolbar');
-            $toolbar->appendButton('Custom', $html);
+            $title = 'See what the site looks like';
+            
+            $url = 'index.php?option=' . $option . '&view=' . $view . '&id=' . $id;
+            
+            $url = substr( Route::_( $url ), strlen( Uri::base( true ) ) + 1 );
+            $url = SiteApplication::getRouter( 'site' )->build( $url )->toString();
+            $url = Uri::root() . substr( $url, strlen( Uri::base( true ) ) + 1 );
+
+            $html = '<a href="' . $url . '" target="_blank" class="btn btn-small"><span class="icon-eye"></span> ' . $title . '</a>';
+            $toolbar = ToolBar::getInstance( 'toolbar' );
+            $toolbar->appendButton( 'Custom', $html );
         }
         else
         {
