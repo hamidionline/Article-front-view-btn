96162b8bed2e0973d389089f87c62b147a5df792
AlekVolsk <AlekVolsk@users.noreply.github.com>
2019-08-19 02:57:43 +0400

    fix Itemid param in url

--- articlefrontviewbtn.php
+++ articlefrontviewbtn.php
@@ -1,4 +1,4 @@
-<?php defined( '_JEXEC' ) or die( 'Restricted access' );
+<?php defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\CMSPlugin;
@@ -13,38 +13,44 @@ class plgSystemArticlefrontviewbtn extends CMSPlugin
     {
         $app = Factory::getApplication();
 
-        if ( Factory::getDocument()->getType() != 'html' )
-        {
+        if (Factory::getDocument()->getType() != 'html') {
             return false;
         }
 
-        if ( !$app->isAdmin() )
-        {
+        if (!$app->isClient('administrator')) {
             return false;
         }
 
-        $option = $app->input->get( 'option' );
-        $view   = $app->input->get( 'view' );
-        $layout = $app->input->get( 'layout' );
-        $id     = $app->input->get( 'id' );
-        
-        if ( $layout == 'edit' && $id && $option && $view )
-        {
+        $option = $app->input->get('option');
+        $view   = $app->input->get('view');
+        $layout = $app->input->get('layout');
+        $id     = $app->input->get('id');
+
+        if ($layout == 'edit' && $id && $option && $view) {
             $title = 'See what the site looks like';
-            
-            $url = 'index.php?option=' . $option . '&view=' . $view . '&id=' . $id;
-            
-            $url = substr( Route::_( $url ), strlen( Uri::base( true ) ) + 1 );
-            $url = SiteApplication::getRouter( 'site' )->build( $url )->toString();
-            $url = Uri::root() . substr( $url, strlen( Uri::base( true ) ) + 1 );
+
+            $menuId = $this->getItemid($option, $view, $id);
+            $url = 'index.php?option=' . $option . '&view=' . $view . '&id=' . $id . ($menuId ? '&Itemid=' . $menuId : '');
+
+            $url = substr(Route::_($url), strlen(Uri::base(true)) + 1);
+            $url = SiteApplication::getRouter('site')->build($url)->toString();
 
             $html = '<a href="' . $url . '" target="_blank" class="btn btn-small"><span class="icon-eye"></span> ' . $title . '</a>';
-            $toolbar = ToolBar::getInstance( 'toolbar' );
-            $toolbar->appendButton( 'Custom', $html );
-        }
-        else
-        {
+            $toolbar = ToolBar::getInstance('toolbar');
+            $toolbar->appendButton('Custom', $html);
+        } else {
             return false;
         }
+    }
+
+    private function getItemid($option, $view, $id)
+    {
+        $items = JFactory::getApplication()->getMenu('site')->getItems('component', $option);
+        foreach ($items as $item) {
+            if ($item->query['view'] === $view && $item->query['id'] === $id) {
+                return $item->id;
+            }
+        }
+        return 0;
     }
 }
